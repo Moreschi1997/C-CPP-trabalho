4d874a3d97cbe25577f7f5aefdb0fe74c9cd79c4
Moreschi1997 <luizfellipe1997.29@gmail.com>
2024-03-03 16:32:26 -0300

    resorce

--- Main.c
+++ Main.c
@@ -5,6 +5,10 @@ void func1(){
     int x;
 }
 int main(){
-    int x = 55;
-    
+    int x = 15;
+    printf("Valor em hex e = %x\n", x);
+    {
+        x++;
+    }
+    printf("Valor atual em hex = %x\n", x);
 }
