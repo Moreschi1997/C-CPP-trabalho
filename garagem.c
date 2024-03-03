26cbf9a56c42937a5711490ca2166d608d10884b
Moreschi1997 <luizfellipe1997.29@gmail.com>
2024-03-03 16:54:44 -0300

    Push

--- garagem.c
+++ garagem.c
@@ -3,6 +3,13 @@
 
 
 int main(){
-    int numerosCarros, nomeCarros [4] [2];
+    int numerosCarros, anoCarros [4] [1];
+
+        for (size_t numerosCarros = 0; numerosCarros < 4; numerosCarros++)
+        {
+            printf("Digite o números de carros na garagem: \n");
+            scanf("%d", [0,4]);
+        }
+        
     
 }
